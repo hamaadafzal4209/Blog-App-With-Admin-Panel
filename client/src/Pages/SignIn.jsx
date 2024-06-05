a56af1618f0c976b18b7f0aad2bb9a45ae2ca647
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-05 04:31:28 -0700

    add redux persist

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -48,7 +48,6 @@ function SignIn() {
   return (
     <div className="my-20">
       <div className="flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold text-3xl dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
@@ -61,7 +60,6 @@ function SignIn() {
             or with Google.
           </p>
         </div>
-        {/* right */}
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
