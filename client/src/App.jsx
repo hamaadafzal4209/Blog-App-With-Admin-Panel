0831d9a3876ed9dc6836dc7e8b22f3e5f20e4e8a
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-06 08:23:42 -0700

    Craete privateroute component and make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./Pages/Dashboard";
 import Projects from "./Pages/Projects";
 import Header from "./Components/Header";
 import FooterComponent from "./Components/Footer";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterComponent />
---
+++ client/src/Components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+function PrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return <div>{currentUser ? <Outlet /> : <Navigate to="/sign-in" />}</div>;
+}
+
+export default PrivateRoute;
