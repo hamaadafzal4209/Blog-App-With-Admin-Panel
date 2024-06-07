89fb4a46eb960c14d583da6884b44e3e66c3e8e6
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-07 03:18:21 -0700

    complete sidebar of the dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Profile from '../Components/Profile';
+import SidebarComponent from '../Components/Sidebar';
 
 function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      Dashboard
+    <div className="flex  flex-col md:flex-row min-h-screen">
+      <div className='w-full md:w-56'>
+        {/* sidebar */}
+        <SidebarComponent />
+      </div>
+ 
+        {tab === 'profile' && <Profile />}
+
     </div>
   )
 }
