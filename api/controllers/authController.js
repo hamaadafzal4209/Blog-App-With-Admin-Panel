7a2ea44adfc60946c225c7b162ad35cb3b3acc1b
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-08 04:30:26 -0700

    create update user route api

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -70,7 +70,7 @@ export const signin = async (req, res, next) => {
 
     res
       .status(200)
-      .cookie("token", token, {
+      .cookie("access_token", token, {
         httpOnly: true,
       })
       .json(rest);
--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 // middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 dotenv.config();
 const PORT = process.env.PORT;
