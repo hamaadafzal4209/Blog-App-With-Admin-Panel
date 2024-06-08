1b2390ae437deed6c0548b72192317246d74b422
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-08 08:08:28 -0700

    create signout api route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/userController.js';
+import { deleteUser, test, updateUser,signout } from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get('/test', test)
-router.put('/update/:userId', verifyToken, updateUser)
-router.delete('/delete/:userId', verifyToken, deleteUser)
+router.get('/test', test);
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
+router.post('/signout', signout)
 
 export default router;
