69258a8c68915426077441713efd12f70daf6f75
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-09 02:15:18 -0700

    modify create api route

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,27 +1,34 @@
 import { errorHandler } from "../utils/error.js";
-import Post from '../models/postModel.js'
+import Post from "../models/postModel.js";
 
 export const create = async (req, res, next) => {
-    if (!req.user.isAdmin) {
-      return next(errorHandler(403, 'You are not allowed to create a post'));
-    }
-    if (!req.body.title || !req.body.content) {
-      return next(errorHandler(400, 'Please provide all required fields'));
-    }
-    const slug = req.body.title
-      .split(' ')
-      .join('-')
-      .toLowerCase()
-      .replace(/[^a-zA-Z0-9-]/g, '');
-    const newPost = new Post({
-      ...req.body,
-      slug,
-      userId: req.user.id,
-    });
-    try {
-      const savedPost = await newPost.save();
-      res.status(201).json(savedPost);
-    } catch (error) {
-      next(error);
-    }
-  };
+  const { title, content, image, category } = req.body;
+  
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all required fields"));
+  }
+
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "");
+
+  const newPost = new Post({
+    title,
+    content,
+    image,
+    category,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
