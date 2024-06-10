0e47c3a94e891312a3086c3833edf0db10344ada
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-10 03:55:41 -0700

    show users to the admin dashboard

---
+++ client/src/Components/DashUsers.jsx
@@ -0,0 +1,148 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Table, Button, Modal } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import {FaCheck,FaTimes} from 'react-icons/fa';
+
+function DashPosts() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [users, setUsers] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [idForDelete, setIdForDelete] = useState("");
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch(`/api/user/getusers`);
+        const data = await res.json();
+        if (res.ok) {
+          setUsers(data.users);
+          if (data.users.length < 9) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchUsers();
+    }
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = users.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleDeletePost = async () => {
+  
+  };
+
+  return (
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+      {currentUser.isAdmin && users.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+            <Table.HeadCell>Date created</Table.HeadCell>
+              <Table.HeadCell>User image</Table.HeadCell>
+              <Table.HeadCell>Username</Table.HeadCell>
+              <Table.HeadCell>Email</Table.HeadCell>
+              <Table.HeadCell>Admin</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+            </Table.Head>
+            {users.map((user) => (
+              <Table.Body key={user._id} className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(user.createdAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                   <img
+                      className="w-10 h-10 object-cover rounded-full mx-auto"
+                      src={user.profilePicture}
+                      alt=""
+                    />
+                  </Table.Cell>
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {user.username}
+                  </Table.Cell>
+                  <Table.Cell>{user.email}</Table.Cell>
+                  <Table.Cell>
+                  {user.isAdmin ? (
+                      <FaCheck className="text-green-500 mx-auto" />
+                    ) : (
+                      <FaTimes className="text-red-500 mx-auto" />
+                    )}
+                  </Table.Cell>
+                  <Table.Cell className="text-red-500 hover:underline cursor-pointer">
+                    <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setIdForDelete(user._id);
+                      }}
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="text-teal-500 mx-auto w-full my-6 text-center"
+            >
+              Show More
+            </button>
+          )}
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+      <Modal
+        show={showModal}
+        size="md"
+        onClose={() => setShowModal(false)}
+        popup
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this product?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handleDeletePost}>
+                {"Yes, I'm sure"}
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default DashPosts;
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import Profile from '../Components/Profile';
 import SidebarComponent from '../Components/Sidebar';
 import DashPosts from '../Components/DashPosts';
+import DashUsers from '../Components/DashUsers';
 
 function Dashboard() {
   const location = useLocation();
@@ -23,6 +24,7 @@ function Dashboard() {
  
         {tab === 'profile' && <Profile />}
         {tab === 'posts' && <DashPosts />}
+        {tab === 'users' && <DashUsers />}
 
     </div>
   )
