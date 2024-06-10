3c6c41df3c3bb4f9f6e23c2e813f12e20f81d73b
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-10 02:25:03 -0700

    create getusers api route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -73,3 +73,45 @@ export const signout = (req, res, next) => {
         next(error);
     }
 };
+
+export const getUsers = async (req, res, next) => {
+    if(!req.user.isAdmin){
+        return next(401,'You are not allowed to see al users');
+    }
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+    
+        const users = await userModel.find()
+          .sort({ createdAt: sortDirection })
+          .skip(startIndex)
+          .limit(limit);
+    
+        const usersWithoutPassword = users.map((user) => {
+          const { password, ...rest } = user._doc;
+          return rest;
+        });
+    
+        const totalUsers = await userModel.countDocuments();
+
+        const now = new Date();
+
+        const oneMonthAgo = new Date(
+          now.getFullYear(),
+          now.getMonth() - 1,
+          now.getDate()
+        );
+        const lastMonthUsers = await userModel.countDocuments({
+          createdAt: { $gte: oneMonthAgo },
+        });
+    
+        res.status(200).json({
+          users: usersWithoutPassword,
+          totalUsers,
+          lastMonthUsers,
+        });
+    } catch (error) {
+        next(error);
+    }
+} 
