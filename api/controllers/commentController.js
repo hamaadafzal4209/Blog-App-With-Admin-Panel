aa7231ab6a15642ebfa844d823736cca45dfed08
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-11 05:09:51 -0700

    create get all comments api route

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -18,3 +18,14 @@ export const createComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comment = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/commentController.js";
+import { createComment, getPostComments } from "../controllers/commentController.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
+router.get('/getpostcomments/:postId',getPostComments);
 
 export default router;
