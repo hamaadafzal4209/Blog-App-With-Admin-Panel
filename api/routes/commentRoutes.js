e89220a08fcbd0bd4daa10246ecc49d040d151e4
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-12 03:10:43 -0700

    complete like functionallity with edit and delete functionallity

--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -4,6 +4,9 @@ import {
   createComment,
   getPostComments,
   likeComment,
+  editComment,
+  deleteComment,
+  getcomments
 } from "../controllers/commentController.js";
 
 const router = express.Router();
@@ -11,5 +14,8 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/getpostcomments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
 
 export default router;
