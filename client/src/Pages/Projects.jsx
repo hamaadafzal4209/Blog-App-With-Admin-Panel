14298d56fcfd66537cc31decfb064f142eda0d01
FrontendFlair <hamaadafzal404@gmail.com>
2024-06-12 08:37:15 -0700

    create projects page

--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,8 +1,11 @@
+import CallToAction from "../Components/CallToAction"
 
 function Projects() {
   return (
-    <div>
-      Projects
+    <div className="min-h-screen max-w-2xl mx-auto w-full flex flex-col items-center justify-center p-3 py-6 gap-6">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="">Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <CallToAction/>
     </div>
   )
 }
