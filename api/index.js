c016952c03f5b6b4f6c8defc6b14d5405f65d11d
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-26 07:41:56 -0700

    deployment

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 // middlewares
@@ -25,11 +26,19 @@ mongoose
     console.log(err.message);
   });
 
+const __dirname = path.resolve();
+
 // import routes
 app.use("/api/user/", userRoutes);
 app.use("/api/auth/", authRoutes);
 app.use("/api/post/", postRoutes);
 app.use("/api/comment/", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // middleware for error handling
 app.use((err, req, res, next) => {
